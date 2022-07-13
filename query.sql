529d5b298305ca0c2c400e0ee40cb268539679dc
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-13 14:56:13 +0200

    select + bonus

--- query.sql
+++ query.sql
@@ -38,19 +38,43 @@ FROM awards
 WHERE description LIKE '% facere %'
 
 -- 7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
--- 
+
+SELECT DISTINCT videogame_id
+FROM category_videogame
+WHERE category_id = 2 OR category_id = 6
+
 -- 8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
--- 
+
+SELECT *
+FROM reviews
+WHERE rating>=2 AND rating<=4
+
 -- 9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
--- 
+
+SELECT *
+FROM videogames
+WHERE DATEPART(year, release_date) = 2020
+
 -- 10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
--- 
+
+SELECT DISTINCT videogame_id
+FROM reviews
+WHERE rating = 5
+
 -- *********** BONUS ***********
 -- 
 -- 11- Selezionare il numero e la media delle recensioni per il videogioco con ID = 412 (review number = 12, avg_rating = 3)
--- 
+
+SELECT COUNT(*) AS numero_recensioni, AVG(rating) AS media
+FROM reviews
+WHERE videogame_id = 412
+
 -- 12- Selezionare il numero di videogame che la software house con ID = 1 ha rilasciato nel 2018 (13)
--- ```
+
+SELECT COUNT(*) AS tot_videogiochi
+FROM videogames
+WHERE software_house_id = 1 AND DATEPART(year, release_date) = 2018
+
 -- 
 -- ------ Query con group by
 -- 
